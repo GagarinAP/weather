a30a272510476b4815dd91149681c1b1f9c36421
GagarinAP <gagarin.web@gmail.com>
2016-06-10 15:50:21 +0300

    clean index and module

--- js/module.js
+++ js/module.js
@@ -1,97 +1,141 @@
-var module1 = ( function(){
-	var z = 100;
+var module = (function(){
+	var data = a();
+	function a(){
+		var result = [];
+		for(var i = 0; i < 4; i++){
+	        result.push({
+	            position: getCity(i),
+	            datetime: setDatetime(),
+	            humidity: setRandom(40, 90),
+	            temperature: setRandom(15, 25),
+	 			wind: setWind()
+	        });
+        }
+        return result;
+	};
+
+	function Randomizer(min,max){
+		return parseInt(Math.random() * (max - min + 1) + min);
+	};
+
+	function setRandom(min, max){
+        var result = [];
+        for (var i = 0; i < 30; i++){
+            result.push(Randomizer(min, max));
+        }
+        return result;
+    };
+
+    function getCity(position) {
+        var cities =['Рівне', 'Дубно', 'Сарни', 'Костопіль'];
+        return cities[position];
+    };
+
+    function setDatetime(){
+    	var result = [];
+    	for(var j = 0; j < 30; j++){
+			var d = new Date(2014, 5, j+1);
+			var options = {				  
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric'				  
+		    };
+		    result.push(d.toLocaleString("ru", options));
+		}
+		return result;
+    };
+
+    function setWind(){
+    	var result = [];
+    	for(var j = 0; j < 30; j++){
+			var windname = ["N","NE","E","SE","S","SW","W","NW"];
+			var vector = parseInt(Randomizer(0,7));				
+			result.push(windname[vector]);			
+		}
+		return result;
+    };
+	
+	var searchByName = function(Position){		
+		var result = [];		
+		for(var i = 0; i < data.length; i++){
+			if(data[i].position == Position){				
+				result.push(data[i]);
+			}
+		}		
+		return result;
+	};
+	function counter(arr){
+	    var ret = {};
+	        arr.forEach( function(elem,i,arr){            
+	            if( elem in this) 
+	                return;
+	            var count = arr.filter( function( elemA ){
+	                return elem === elemA;
+	            });
+	            this[elem] = count.length;              
+	        }, ret);
+	    return ret;
+	}
+	var AverageArray = function(arr){
+		var result=[]
+		  , averagehumidity = 0
+      	  , averagetemperature = 0
+      	  , averagewind=[]
+      	  , max = 0
+      	  , position;      	
+
+		for(var i = 0; i < arr.length; i++){        
+	        for(var j = 0; j < 30; j++){
+	          averagehumidity += arr[i].humidity[j];
+	          averagetemperature += arr[i].temperature[j];
+	          averagewind.push(arr[i].wind[j]);         
+	        }	 
+	        result[0] = averagehumidity /= 30;
+	    	result[1] = averagetemperature /= 30;              
+	    }
+
+	    var maxi = counter(averagewind);
+	    for(key in maxi){
+	        if(maxi[key] > max){
+	            max = maxi[key];
+	            position = key;            
+	        }             
+	    }
+
+	    result[2] = position;
+	    
+		return result;
+    };
+    var MaxAndPosition = function(arr){
+		var result=[]		      	  
+      	  , max = 0
+      	  , max1 = 0
+      	  , position1
+      	  , position;      	
+
+		for(var i = 0; i < arr.length; i++){        
+	        for(var j = 0; j < 30; j++){
+	        	if(arr[i].humidity[j] > max){
+	        		max = arr[i].humidity[j];
+	        		position = arr[i].datetime[j];
+	        	}
+	        	if(arr[i].temperature[j] > max1){
+	        		max1 = arr[i].temperature[j];
+	        		position1 = arr[i].datetime[j];
+	        	}	          	                   
+	        }	                     
+	    }
+	    result[0] = max;
+	    result[1] = position;  
+	    result[2] = max1;
+	    result[3] = position1;
+	    
+		return result;
+    };
 	return {
-		z: z
+		data:data,
+		MaxAndPosition:MaxAndPosition,
+		AverageArray:AverageArray,
+		searchByName:searchByName
 	};
 })();
-
-var module = ( function(module1){	
-	console.log(module1.z);
-	var arr = [
-		{
-			pos: "",
-			date: "",
-			humidity: "",
-			temperature: 0,
-			wind: ""
-		}
-	];
-	/*var arr = [
-	{
-		pos: 'Рівне',
-		date: {},
-		humidity: {},		
-		temperature: {},
-		wind: {}
-	},
-	{
-		pos: 'Дубно',
-		date: {},
-		humidity: {},		
-		temperature: {},
-		wind: {}
-	},
-	{
-		pos: 'Костопіль',
-		date: {},
-		humidity: {},		
-		temperature: {},
-		wind: {}
-	},
-	{
-		pos: 'Сарни',
-		date: {},
-		humidity: {},		
-		temperature: {},
-		wind: {}
-	}
-	]; */
-	for(var i = 0; i < arr.length; i++){
-			for(var j = 0; j < 30; j++){
-				var d = new Date(20*module1.z, 5, j+1);
-				var options = {				  
-				  year: 'numeric',
-				  month: 'long',
-				  day: 'numeric'				  
-				};
-			arr[i].date[j] = d.toLocaleString("ru", options);					
-		}
-	};
-	for(var i = 0; i < arr.length; i++){
-			for(var j = 0; j < 30; j++){							
-				arr[i].humidity[j] = parseInt(Math.random() * 40 + 60);			
-		}
-	};	
-	for(var i = 0; i < arr.length; i++){
-			for(var j = 0; j < 30; j++){							
-				arr[i].temperature[j] = parseInt(Math.random() * 20 + 10);			
-		}
-	};
-	for(var i = 0; i < arr.length; i++){
-		for(var j = 0; j < 30; j++){
-			var result = ["ПнСх","ПдЗх","ПнЗх","ПдСх"];
-			var key = parseInt(Math.random() * 4);				
-			arr[i].wind[j] = result[key];			
-		}
-	};
-	var GlobalArray = function(){
-		var result = [];
-		for(var i = 0; i < arr.length; i++){
-			result.push(arr[i]);
-		}
-		return result;
-	};
-	var searchByName = function(Position){		
-		var result = [];
-		for(var i = 0; i < arr.length; i++){
-			if(arr[i].pos == Position){				
-				result.push(arr[i]);
-			}
-		}
-		return result;
-	};
-	return{		
-		GlobalArray : GlobalArray,
-		searchByName : searchByName
-	}
-})(module1);
---
+++ js/show.js
@@ -0,0 +1,56 @@
+function ShowAll(){
+  var GlobalArray = module.data;         
+  var array = "";     
+      for(var i = 0; i < GlobalArray.length; i++){        
+        array += '<table class="table table-bordered"><caption>' + '<h1>' + GlobalArray[i].position + '</h1>' + '</caption><thead><tr><th>#</th><th>Дата</th><th>Вологість (%)</th><th>Температура (&#176;C)</th><th>Вітер (напрям)</th></tr></thead>';
+        for(var j = 0; j < 30; j++){
+          array += '<tbody><tr><th scope="row">' + (j + 1) + '</th><td>' + GlobalArray[i].datetime[j] + '</td><td>' + GlobalArray[i].humidity[j] + '</td><td>' + GlobalArray[i].temperature[j] + '</td><td>' + GlobalArray[i].wind[j] + '</td></tbody>';
+        }
+      }
+  array += '</table>';
+  $('#ShowAll').html(array);
+}
+  
+function Search(){
+      var NameFromSearchForm = $('#inputName').val();     
+      var array = "";
+
+      var form = document.forms[0];
+      var select = form.elements.weather;      
+      
+      var GlobalArray = module.data;      
+
+      if(NameFromSearchForm){     
+          var ArrayFromSearchName = module.searchByName(NameFromSearchForm);
+      }
+      var AverageAllData = module.AverageArray(ArrayFromSearchName);      
+      var Max = module.MaxAndPosition(ArrayFromSearchName);
+      if(select.value > 2){
+        for(var i = 0; i < ArrayFromSearchName.length; i++){
+          array += '<table class="table table-bordered"><caption><h1>' + ArrayFromSearchName[i].position + '</h1></caption><thead><tr><th>Вітер (напрям)</th></tr></thead>'; 
+        }               
+        array += '<tbody><tr><td>' + AverageAllData[2] + '</td></tr></tbody><table>';           
+      } else if (select.value == 2){
+        for(var i = 0; i < ArrayFromSearchName.length; i++){
+          array += '<table class="table table-bordered"><caption><h1>' + ArrayFromSearchName[i].position + '</h1></caption><thead><tr><th>Дата</th><th>Температура (&#176;C)</th></tr></thead>'; 
+        }               
+        array += '<tbody><tr><td>'+Max[3]+'</td><td>' + Max[2] + '</td></tr></tbody><table>';             
+      } else {        
+        for(var i = 0; i < ArrayFromSearchName.length; i++){
+          array += '<table class="table table-bordered"><caption><h1>' + ArrayFromSearchName[i].position + '</h1></caption><thead><tr><th>Дата</th><th>Вологість (%)</th></tr></thead>'; 
+        }               
+        array += '<tbody><tr><td>'+Max[1]+'</td><td>' + Max[0] + '</td></tr></tbody><table>';       
+      }
+      $('#ResultSearchByName').html(array);
+}
+  
+function AverageAll(){
+      var GlobalArray = module.data;
+      var AverageAllData = module.AverageArray(GlobalArray);
+      var array = "";      
+
+      array += '<table class="table table-bordered"><thead><tr><th>Вологість (%)</th><th>Температура (&#176;C)</th><th>Вітер (напрям)</th></tr></thead>';
+      array += '<tbody><tr><td>' + parseInt(AverageAllData[0]) + '</td><td>' + parseInt(AverageAllData[1]) + '</td><td>' + AverageAllData[2] + '</td></tr></tbody><table>';      
+      $('#AverageResultAll').html(array);
+}
+  
